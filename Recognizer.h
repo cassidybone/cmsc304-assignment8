75e37110adb9ef33daa9db14413a3413e1f59757
cassidybone <bohnc@vcu.edu>
2025-12-13 06:49:47 -0500

    w

--- Recognizer.h
+++ Recognizer.h
@@ -1,5 +1,5 @@
-#ifndef TOKENIZER_H
-#define TOKENIZER_H
+#ifndef RECOGNIZER_H
+#define RECOGNIZER_H
 
 #include <stddef.h>
 #include <stdio.h>
@@ -10,20 +10,18 @@
 
 int isTerm(void);
 int isExpression(void);
-
-
 int isAssignment(void);
 int isReturn(void);
 int isWhileLoop(void);
 int isStatement(void);
 int isStatementList(void);
-
-
 int isBody(void);
 int isArgDecl(void);
 int isHeader(void);
-
-
 int isFunction(void);
+int branch();
+TokenType unString(char *s);
+void shorten(char *aString);
+int toLex(char *line);
 
 #endif
