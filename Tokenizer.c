c39639aff5407e3823921f3c3791fdd2b4aa397b
cassidybone <bohnc@vcu.edu>
2025-12-13 02:51:45 -0500

    asdf

--- Tokenizer.c
+++ Tokenizer.c
@@ -10,34 +10,81 @@
 
 char* printToken(Token* aToken);
 
+TokenType identify(char* lexeme){
+    
+    lexeme = rstrip(lexeme);
+
+    // Check if reserved words
+    if (strcmp(lexeme, "while") == 0)  return WHILE_KEYWORD;
+    if (strcmp(lexeme, "return") == 0) return RETURN_KEYWORD;
+    if (strcmp(lexeme, "int") == 0 || strcmp(lexeme, "void") == 0) return VARTYPE;
+
+    // Check if Symbol
+    if (strcmp(lexeme, "(") == 0) return LEFT_PARENTHESIS;
+    if (strcmp(lexeme, ")") == 0) return RIGHT_PARENTHESIS;
+    if (strcmp(lexeme, "{") == 0) return LEFT_BRACKET;
+    if (strcmp(lexeme, "}") == 0) return RIGHT_BRACKET;
+    if (strcmp(lexeme, "=") == 0) return EQUAL;
+    if (strcmp(lexeme, ",") == 0) return COMMA;
+    if (strcmp(lexeme, ";") == 0) return EOL;
+    if (strcmp(lexeme, "+") == 0 || strcmp(lexeme, "*") == 0 ||
+        strcmp(lexeme, "==") == 0 || strcmp(lexeme, "!=") == 0 ||
+        strcmp(lexeme, "%") == 0) return BINOP;
+
+    // Check if number
+    if (!isdigit((unsigned char)lexeme[0]))  return NUMBER;
+
+    // Otherwise IDENTIFIER 
+    return IDENTIFIER;
+
+
+}
 
 Lex* toToken(Char* line, int* count){
     
+
     int i = 0;
     Lex[] slot = malloc(sizeof(Lex)*5);
     while (line[i] != '\0') { // check each character at a time until null terminator
-        char temp[MAX_LINE_SIZE] = line[i];
+        char temp[MAX_LINE_SIZE];
+        temp[i] = line[i];
         if (isalpha((unsigned char)line[i])) {
             while(((isalpha((unsigned char)line[i+1])) || (isdigit((unsigned char)line[i+1])))){ //while a character or letter string collect
             i++;
             temp[i]= line[i];
             }
         }
-        else if (isdigit((unsigned char)c)) {
-            // start or continue NUMBER
+        else if (isdigit((unsigned char)line[i])) {
+            while(((isdigit((unsigned char)line[i+1])))){ //while string of digits collect
+            i++;
+            temp[i]= line[i];
+            }
         }
-        else if (isspace((unsigned char)c)) {
-            // delimiter
+        else if (isspace((unsigned char)line[i])) { // if space loop
+            i++;
+            continue;
         }
         else {
-            // symbol
+            while(!((isspace((unsigned char)line[i])) || (isalpha((unsigned char)line[i+1])) || (isdigit((unsigned char)line[i+1])))){
+                i++;
+                temp[i]= line[i];
+            }
         }
 
-        slot[count] = struct Lex
-        *count++; //increment number of tokens found
+        Lex newLex;
+        strcpy(newLex.lexeme, temp);
+        newLex.type = identify(temp);
+        slot[*count] = newLex;
+        (*count)++;                                                 //increment number of tokens found
+        if((*count)%5 == 0){
+            slot = realloc(slot, sizeof(Lex)*(*count)+5);
+        }
 
 
     }
+
+    return slot;
+    
 }
 
 
